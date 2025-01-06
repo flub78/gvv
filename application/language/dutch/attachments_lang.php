df3b6d3fac75dd82930dff37f11d16b2d7652383
Flub78 <frederic.peignot@free.fr>
2025-01-06 14:50:05 +0100

    Attachments translations

---
+++ application/language/dutch/attachments_lang.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * GVV Dutch translation
+ * TODO : translate
+*/
+
+# Attachments view
+
+$lang['gvv_attachments_title'] = "Rechtvaardiging";
+
+$lang['gvv_attachments_field_filename'] = "Bestandsnaam";
+$lang['gvv_attachments_field_description'] = "Beschrijving";
+$lang['gvv_attachments_field_file'] = "Bestand";
+
+$lang['gvv_vue_attachments_short_field_referenced_table'] = "Tafel";
+$lang['gvv_vue_attachments_short_field_referenced_id'] = "Element";
+$lang['gvv_vue_attachments_short_field_description'] = "Beschrijving";
+$lang['gvv_vue_attachments_short_field_file'] = "Bestand";
--- application/language/english/attachments_lang.php
+++ application/language/english/attachments_lang.php
@@ -1,18 +1,18 @@
 <?php
 /*
- * GVV French translation
+ * GVV English translation
  * TODO : translate
 */
 
 # Attachments view
 
-$lang['gvv_attachments_title'] = "Justificatifs";
+$lang['gvv_attachments_title'] = "Attachments";
 
-$lang['gvv_attachments_field_filename'] = "Nom de fichier";
+$lang['gvv_attachments_field_filename'] = "File name";
 $lang['gvv_attachments_field_description'] = "Description";
-$lang['gvv_attachments_field_file'] = "Fichier";
+$lang['gvv_attachments_field_file'] = "File";
 
 $lang['gvv_vue_attachments_short_field_referenced_table'] = "Table";
 $lang['gvv_vue_attachments_short_field_referenced_id'] = "Element";
 $lang['gvv_vue_attachments_short_field_description'] = "Description";
-$lang['gvv_vue_attachments_short_field_file'] = "Fichier";
+$lang['gvv_vue_attachments_short_field_file'] = "File";
