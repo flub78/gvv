81d2eacbb46696c70c02ab5fccb2aae1a50e4681
Flub78 <frederic.peignot@free.fr>
2025-01-15 09:01:42 +0100

    migration user_roles

---
+++ application/migrations/025_roles_per_section.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ *    GVV Gestion vol à voile
+ *    Copyright (C) 2011  Philippe Boissel & Frédéric Peignot
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	  Script de migration de la base
+ */
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+/**
+ * Passe les compteurs de tickets en décimal
+ *    
+ * @author frederic
+ *
+ */
+class Migration_Roles_per_section extends CI_Migration {
+
+	protected $migration_number;
+
+	/**
+	 *
+	 * Constructor
+	 *
+	 * Affiche header et menu
+	 */
+	function __construct() {
+		parent::__construct();
+		$this->migration_number = 25;
+		$this->load->library('database');
+	}
+
+	/*
+	 * Execute an array of sql requests
+	 */
+	private function run_queries($sqls = array()) {
+		$errors = 0;
+		foreach ($sqls as $sql) {
+			gvv_info("Migration sql: " . $sql);
+			if (!$this->db->query($sql)) {
+				gvv_error("Migration error: " . $this->db->error()['message']);
+				$errors += 1;
+			}
+		}
+		return $errors;
+	}
+
+
+	/**
+	 * Apply the migration
+	 */
+	public function up() {
+		$errors = 0;
+
+		$filename = getcwd() . '/application/migrations/types_roles.sql';
+		$res = $this->database->sqlfile($filename);
+
+		$filename = getcwd() . '/application/migrations/user_roles_per_section.sql';
+		$res = $this->database->sqlfile($filename);
+
+		gvv_info("Migration database up to " . $this->number . ", errors=$errors");
+		return !$errors;
+	}
+
+	/**
+	 * Reverse the migration
+	 */
+	public function down() {
+		$errors = 0;
+		$sqls = array(
+			"DROP TABLE IF EXISTS user_roles_per_section",
+			"DROP TABLE IF EXISTS types_roles"
+
+		);
+		$errors += $this->run_queries($sqls);
+
+		gvv_info("Migration database down to " . ($this->migration_number - 1) . ", errors=$errors");
+
+		return !$errors;
+	}
+}
