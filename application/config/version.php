4d075b27426d36980b1b1cb821fcb3bb7098b0ca
Flub78 <frederic.peignot@free.fr>
2025-04-05 08:09:36 +0200

    Modification mineure sur les scripts d'installation

--- application/config/version.php
+++ application/config/version.php
@@ -1,6 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-$config['commit'] = '72b798e';
-$config['commit_date'] = '31/03/2025 09:34:31';
-$config['commit_message'] = "correction du nombre d'éléments du bandeau bas des journaux lors de la selection d'une section";
-
+defined('BASEPATH') or exit('No direct script access allowed');
+$config['commit'] = 'b2838cbe0b0c95bd7f5568da8694827cfa897bb6';
+$config['commit_date'] = '05/04/2025 08:00:31';
+$config['commit_message'] = "Modification mineure sur les scripts d'installation";
