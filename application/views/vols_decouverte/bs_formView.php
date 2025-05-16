04c57a6c178d33fc03eaaad88ff7df6821a7761d
Flub78 <frederic.peignot@free.fr>
2025-05-16 07:26:34 +0200

    Vols découverte MVP

--- application/views/vols_decouverte/bs_formView.php
+++ application/views/vols_decouverte/bs_formView.php
@@ -43,25 +43,97 @@ echo form_hidden('saisie_par', $saisie_par, '');
 
 
 // echo validation_errors();
-echo ($this->gvvmetadata->form('vols_decouverte', array(
-	'date_vente' => $date_vente,
-	'product' => $product,
-	'beneficiaire' => $beneficiaire,
-	'de_la_part' => $de_la_part,
-	'occasion' => $occasion,
-	'beneficiaire_email' => $beneficiaire_email,
-	'urgence' => $urgence,
-	// 'date_planning' => $date_planning,
-	// 'time_planning' => $time_planning,
-	'date_vol' => $date_vol,
-	// 'time_vol' => $time_vol,
-	'pilote' => $pilote,
-	'airplane_immat' => $airplane_immat,
-	'cancelled' => $cancelled,
-	'paiement' => $paiement,
-	'participation' => $participation,
 
-)));
+$url = current_url();
+if (strpos($url, 'pre_flight')) {
+	$modification_type = 'pre_flight';
+} elseif (strpos($url, 'done')) {
+	$modification_type = 'done';
+} else {
+	$modification_type = 'edit';
+}
+if ($modification_type == 'edit') {
+	echo ($this->gvvmetadata->form('vols_decouverte', array(
+		'date_vente' => $date_vente,
+		'product' => $product,
+		'beneficiaire' => $beneficiaire,
+		'de_la_part' => $de_la_part,
+		'occasion' => $occasion,
+		'beneficiaire_email' => $beneficiaire_email,
+		'urgence' => $urgence,
+		'date_vol' => $date_vol,
+		'pilote' => $pilote,
+		'airplane_immat' => $airplane_immat,
+		'cancelled' => $cancelled,
+		'paiement' => $paiement,
+		'participation' => $participation,
+
+	)));
+} else {
+
+
+	echo form_hidden('date_vente', date_db2ht($date_vente), '');
+	echo form_hidden('product', $product, '');
+	echo form_hidden('beneficiaire', $beneficiaire, '');
+	echo form_hidden('de_la_part', $de_la_part, '');
+	echo form_hidden('occasion', $occasion, '');
+	echo form_hidden('beneficiaire_email', $beneficiaire_email, '');
+	echo form_hidden('cancelled', $cancelled, '');
+	echo form_hidden('paiement', $paiement, '');
+	echo form_hidden('participation', $participation, '');
+
+	if ($modification_type == 'pre_flight') {
+		echo form_hidden('date_vol', $date_vol, '');
+		echo form_hidden('pilote', $pilote, '');
+		echo form_hidden('airplane_immat', $airplane_immat, '');
+
+		?>
+	<div class="d-flex flex-wrap">
+		<div class="m-2 ">
+			<div class="mb-2 ">Numéro: <?= $id ?> </div>
+			<div class="mb-2 ">Date de vente: <?= date_db2ht($date_vente) ?> </div>
+			<div class="mb-2 ">Description: <?= $description ?></div>
+			<div class="mb-2 ">Bénéficiaire: <?= $beneficiaire ?></div>
+		</div>
+		<div class="m-2 ">
+			<div class="mb-2 ">De la part de: <?= $de_la_part ?></div>
+			<div class="mb-2 ">Email bénéficiaire: <?= $beneficiaire_email ?></div>
+		</div>
+
+	</div>
+<?php
+
+		echo ($this->gvvmetadata->form('vols_decouverte', array(
+			'urgence' => $urgence,
+		)));
+	} else {
+		// done
+		echo form_hidden('urgence', $urgence, '');
+
+		?>
+	<div class="d-flex flex-wrap">
+		<div class="m-2 ">
+			<div class="mb-2 ">Numéro: <?= $id ?> </div>
+			<div class="mb-2 ">Date de vente: <?= date_db2ht($date_vente) ?> </div>
+			<div class="mb-2 ">Description: <?= $description ?></div>
+			<div class="mb-2 ">Bénéficiaire: <?= $beneficiaire ?></div>
+		</div>
+		<div class="m-2 ">
+			<div class="mb-2 ">De la part de: <?= $de_la_part ?></div>
+			<div class="mb-2 ">Email bénéficiaire: <?= $beneficiaire_email ?></div>
+			<div class="mb-2 ">Contact en cas d'urgence: <?= $urgence ?></div>
+		</div>
+
+	</div>
+<?php
+
+		echo ($this->gvvmetadata->form('vols_decouverte', array(
+			'date_vol' => $date_vol,
+			'pilote' => $pilote,
+			'airplane_immat' => $airplane_immat,
+		)));
+	}
+}
 
 echo validation_button($action);
 echo form_close();
