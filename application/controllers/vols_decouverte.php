cfe2ff88337d3456c35e1833543dc7adbcd33d48
Flub78 <frederic.peignot@free.fr>
2025-05-24 18:20:47 +0200

    Changement image de fond vols de découverte

--- application/controllers/vols_decouverte.php
+++ application/controllers/vols_decouverte.php
@@ -222,7 +222,7 @@ class Vols_decouverte extends Gvv_Controller {
         // disable auto-page-break
         $pdf->SetAutoPageBreak(false, 0);
         // set background image
-        $img_file = image_dir() . "vd_recto.jpg";
+        $img_file = image_dir() . "Bon-Bapteme.png";
         $pdf->Image($img_file, 0, 0, 210, 150, '', '', '', false, 300, '', false, false, 0);
         // restore auto-page-break status
         $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
