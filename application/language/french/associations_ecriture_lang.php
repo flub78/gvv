f4d6d6c1c6e8b90cdd6d19b042b18090205cc3d4
flub78 <frederic.peignot@free.fr>
2025-08-10 17:27:18 +0200

    Rapprochements: ressource associations_ecriture, Github copilot generated code

---
+++ application/language/french/associations_ecriture_lang.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * GVV French translation
+ */
+
+$lang['gvv_associations_ecriture_field_string_releve'] = "Chaîne relevé";
+$lang['gvv_associations_ecriture_field_id_compte_gvv'] = "Compte GVV";
+$lang['gvv_associations_ecriture_title_associations'] = "Associations des relevés de compte";
+$lang['gvv_associations_ecriture_title_association'] = "Association de relevé de compte";
+
+$lang['gvv_vue_associations_ecriture_short_field_string_releve'] = "Chaîne du relevé";
+$lang['gvv_vue_associations_ecriture_short_field_id_compte_gvv'] = "Compte GVV";
