529846b16de471b4632427c4cf4cf7537a59f0a0
flub78 <frederic.peignot@free.fr>
2025-10-14 11:31:06 +0200

    Nom et prénoms des membres sur 80 caractères

--- application/config/migration.php
+++ application/config/migration.php
@@ -24,7 +24,7 @@ if (! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['migration_enabled'] = true;
 
-$config['migration_version'] = 39;
+$config['migration_version'] = 40;
 
 /* End of file migration.php */
 /* Location: .application/config/migration.php */
---
+++ application/migrations/040_extend_membre_name_fields.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * GVV Migration
+ * Script de migration de la base - extend_membre_name_fields
+ * 
+ * Augmente la taille des champs mnom et mprenom de la table membres de 25 à 80 caractères
+ * pour permettre des noms et prénoms plus longs.
+ */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Migration_Extend_membre_name_fields extends CI_Migration {
+
+    protected $migration_number;
+
+    function __construct() {
+        parent::__construct();
+        $this->migration_number = 40;
+    }
+
+    private function run_queries($sqls = array()) {
+        $errors = 0;
+        foreach ($sqls as $sql) {
+            gvv_info("Migration sql: " . $sql);
+            if (!$this->db->query($sql)) {
+                gvv_error("Migration error: " . $this->db->error()['message']);
+                $errors += 1;
+            }
+        }
+        return $errors;
+    }
+
+    public function up() {
+        $errors = 0;
+
+        $sqls = array(
+            "ALTER TABLE `membres` CHANGE `mnom` `mnom` VARCHAR(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Nom du membre'",
+            "ALTER TABLE `membres` CHANGE `mprenom` `mprenom` VARCHAR(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Prénom du membre'"
+        );
+
+        $errors += $this->run_queries($sqls);
+        gvv_info("Migration database up to " . $this->migration_number . ", errors=$errors");
+
+        return !$errors;
+    }
+
+    public function down() {
+        $errors = 0;
+        $sqls = array(
+            "ALTER TABLE `membres` CHANGE `mnom` `mnom` VARCHAR(25) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Nom du membre'",
+            "ALTER TABLE `membres` CHANGE `mprenom` `mprenom` VARCHAR(25) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Prénom du membre'"
+        );
+
+        $errors += $this->run_queries($sqls);
+        gvv_info("Migration database down to " . ($this->migration_number - 1) . ", errors=$errors");
+
+        return !$errors;
+    }
+}
